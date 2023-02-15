9205d78597b5938b72957645e48821fcf2d8c1ee
david <davidleon_06@hotmail.com>
2023-02-15 11:06:10 +0100

    Using context with useContext hook from react in sustitution of the consumer

--- my-react-loggin-app/src/components/MainHeader/Navigation.js
+++ my-react-loggin-app/src/components/MainHeader/Navigation.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../../context/auth-context";
 
 const Navigation = (props) => {
+  const kontext = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(kontext) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {kontext.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {kontext.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {kontext.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {kontext.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {kontext.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {kontext.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
